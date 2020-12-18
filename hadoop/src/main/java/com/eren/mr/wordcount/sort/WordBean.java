80e1e9e3c2de8a06d43eed2a8e164b8b7c0ee102
eren <442992034@qq.com>
2020-12-18 13:26:38 +0800

    map reduce 案例

---
+++ hadoop/src/main/java/com/eren/mr/wordcount/sort/WordBean.java
@@ -0,0 +1,24 @@
+package com.eren.mr.wordcount.sort;
+
+public class WordBean {
+
+    private String word;
+
+    private Long num;
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    public Long getNum() {
+        return num;
+    }
+
+    public void setNum(Long num) {
+        this.num = num;
+    }
+}
